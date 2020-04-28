584ce10ada3b313e6dd44fd2417bd057ff9713c9
Sergey Shirobokov <shir994@gmail.com>
2020-04-28 12:23:07 +0200

    make path to geofile absolute

--- set_paths_flux.sh
+++ set_paths_flux.sh
@@ -2,4 +2,5 @@
 
 export PRODUCTION_FOLDER=docker_test
 export OUTFILE=docker_test.root
-export MAGNET_GEO=magnet_geo_to_test_oliver.root
+export MAGNET_GEO_FULL_PATH=/eos/experiment/ship/data/magnet_geofiles/magnet_geo_to_test_oliver.root
+export MAGNET_GEO=`basename $MAGNET_GEO_FULL_PATH`

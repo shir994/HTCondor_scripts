d76e318d0c0ed90b9a2e403c72a05a7774c21592
Sergey Shirobokov <shir994@gmail.com>
2020-05-15 17:46:36 +0200

    change env set file

--- set_global_envs.sh
+++ set_global_envs.sh
@@ -1,4 +1,4 @@
 export EOS_DIR=/eos/experiment/ship/user/USERNAME
 export SHIP_CVMFS_SETUP_FILE=/cvmfs/ship.cern.ch/SHiP-2020/latest/setUp.sh
 export CONDOR_ROOT=PATH_TO_WHERE_YOU_HAVE_CONDOR_FILES
-
+export ALIBUILD_WORK_DIR=PATH_TO_YOUR__SW__DIRECTORY
--- set_paths_flux.sh
+++ set_paths_flux.sh
@@ -4,4 +4,3 @@ export PRODUCTION_FOLDER=docker_flux_test
 export OUTFILE=flux.root
 export MAGNET_GEO_FULL_PATH=/eos/experiment/ship/data/magnet_geofiles/magnet_geo_to_test_oliver.root
 export MAGNET_GEO=`basename $MAGNET_GEO_FULL_PATH`
-export ALIBUILD_WORK_DIR=~/sw
